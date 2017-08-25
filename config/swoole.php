a63674ab94628b1dc8e09c42aef6511a1c0f0894
Albert Chen <albert.cht@gmail.com>
2017-08-25 11:25:37 +0800

    change default ws port to 9001

--- config/swoole.php
+++ config/swoole.php
@@ -3,7 +3,7 @@
 return [
     'websocket' => [
         'server' => env('WS_SERVER', '0.0.0.0'),
-        'port' => env('WS_PORT', 9000),
+        'port' => env('WS_PORT', 9001),
         'heartbeat_push' => env('WS_HEARTBEAT_PUSH', true),
         'heartbeat_server_interval' => env('WS_HARTBEAT_SERVER_INTERVAL', 30),
         'max_online_users' => env('WS_MAX_ONLINE_USERS', 2048),
