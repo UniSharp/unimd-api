0ce40ff3da1f817b94471f97e42a5406e9006c2a
Albert Chen <albert.cht@gmail.com>
2017-09-03 21:52:41 +0800

    add note rest api

---
+++ app/Http/Controllers/API/V1/NoteController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Note;
+
+class NoteController extends Controller
+{
+    public function list()
+    {
+        $notes = auth()->user()->notes()->paginate();
+
+        return response()->json($notes);
+    }
+
+    public function get($id)
+    {
+        $note = Note::findOrFail($id);
+        if ($note->author_id !== auth()->id()) {
+            throw new AuthorizationException('Illegal Access');
+        }
+
+        return response()->json($note);
+    }
+
+    public function create(Request $request)
+    {
+        $inputs = $request->only(['title']);
+        $inputs['author_id'] = auth()->id();
+        // create a new empty note
+        $note = Note::create($inputs);
+
+        return response()->json($note);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $inputs = $request->intersect(['title', 'alias']);
+        $note = Note::findOrFail($id);
+        if ($note->author_id !== auth()->id()) {
+            throw new AuthorizationException('Illegal Access');
+        }
+        // update a note
+        $note->update($inputs);
+
+        return response()->json($note);
+    }
+
+    public function delete($id)
+    {
+        $note = Note::findOrFail($id);
+        if ($note->author_id !== auth()->id()) {
+            throw new AuthorizationException('Illegal Access');
+        }
+        // update a note
+        $note->delete();
+
+        return response()->json([
+            'message' => 'success'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,13 +16,18 @@ $app->group(['prefix' => 'api/v1', 'namespace' => 'API\V1'], function ($app) {
         return $app->version();
     });
 
+    // authentication
     $app->post('/register', ['as' => 'auth.register', 'uses' => 'AuthController@register']);
     $app->post('/login', ['as' => 'auth.login', 'uses' => 'AuthController@login']);
     $app->post('/logout', ['as' => 'auth.logout', 'uses' => 'AuthController@logout']);
 
-    $app->group(['prefix' => '/prefix', 'middleware' => 'auth:api'], function () use ($app) {
-        $app->get('/', function () use ($app) {
-            dd(auth()->user());
-        });
+    // auth protected routes
+    $app->group(['prefix' => '/note', 'middleware' => 'auth:api'], function () use ($app) {
+        // notes
+        $app->get('/list', ['as' => 'note.list', 'uses' => 'NoteController@list']);
+        $app->get('/{note}', ['as' => 'note.get', 'uses' => 'NoteController@get']);
+        $app->post('/create', ['as' => 'note.create', 'uses' => 'NoteController@create']);
+        $app->put('/{note}', ['as' => 'note.update', 'uses' => 'NoteController@update']);
+        $app->delete('/{note}', ['as' => 'note.delete', 'uses' => 'NoteController@delete']);
     });
 });
